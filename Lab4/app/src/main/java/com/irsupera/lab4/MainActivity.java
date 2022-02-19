398096619bb0ed3fddaf29cdaae51a14c9f92b86
irsupera <97858090+irsupera@users.noreply.github.com>
2022-02-18 22:14:50 -0500

    Lab4
    Added files for lab 4 - android widgets/layouts

---
+++ Lab4/app/src/main/java/com/irsupera/lab4/MainActivity.java
@@ -0,0 +1,25 @@
+package com.irsupera.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_points);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.afb_scores, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+    }
+}
